6122f9cb7891135a97d54be17db18ff9d6d789dc
Chris Malin <csmalin@gmail.com>
2015-06-01 05:29:53 +0000

    wip: tile layout

--- public/javascripts/gdopener.js
+++ public/javascripts/gdopener.js
@@ -18,16 +18,16 @@ $(function () {
     $('.row.history ul').prepend(li);
     $('.row.history ul li:last-child').remove();
     if (data.DOOR_ID === 0) {
-      $('.percentage-slider').toggleClass('hidden', data.isOpen);
+      $('.right.percentage-slider').toggleClass('hidden', data.isOpen);
       $('button.gdopener').text(data.STATE ? 'Go!' : 'Open!');
-      $('.status').text('State: ' + (data.STATE ? 'Open' : 'Closed'));
+      // $('.status').text("State: " + (data.STATE ? "Open" : "Closed"));
     }
   });
 
   $('.gdopener').on('click', function () {
     triggerDoor();
-    var $slider = $('.percentage-slider');
-    if (!$slider.hasClass('hide')) {
+    var $slider = $('.right.percentage-slider');
+    if (!$slider.hasClass('hidden')) {
       var percentage = parseInt($('#sliderOutput').text()) / 100;
       if (percentage < 1) {
         var stopTime = 13500 * percentage;
